3f91b222b98cb2bedf173bc8aeea3bc494ee6179
Hoang Pham <hoangpm6814@gmail.com>
2020-07-27 23:43:01 +0700

    Room-letter done, need perfect
    Not choose room like cinema
    Not add constraint

--- routes/room-letters/index.js
+++ routes/room-letters/index.js
@@ -11,26 +11,28 @@ User = require("../../models/user.model")
 router.get("/views", async (req, res) => {
   try {
     let _roomletters = await RoomLetter.find({ isDeleted: false })
-    // const _customers = await Customer.find({ isDeleted: false })
+    const _rooms = await Room.find({ isDeleted: false })
+    const _customers = await Customer.find({ isDeleted: false })
     const _customerTypes = await CustomerType.find({ isDeleted: false })
     if (_roomletters && _roomletters.length > 0) {
       for (let i = 0; i < _roomletters.length; i++) {
         const createdByUser = await User.findById(_roomletters[i].createdBy)
-        _roomletters[i].createdByUser = createdByUser
-          ? createdByUser.name
-          : ""
-          // _roomletters[i].dayCheckIn = (new Date(_roomletters[i].dayCheckIn)).toLocaleDateString().split('/').reverse().join('-');
-          // _roomletters[i].dayCheckOut = (new Date(_roomletters[i].dayCheckOut)).toLocaleDateString().split('/').reverse().join('-');
-          console.log(i);
+        _roomletters[i].createdByUser = createdByUser ? createdByUser.name : ""
+
+        const cus = await Customer.findById(_roomletters[i].customerId)
+        _roomletters[i].cus = cus ? cus.name : ""
+
+        const room = await Room.findById(_roomletters[i].roomId)
+        _roomletters[i].room = room ? room.nameOfRoom : ""
       }
     }
-    console.log(_roomletters[1])
 
     res.render("pages/room-letters/index", {
       layout: "layout",
       data: _roomletters,
-      // data: _customers,
+      dataRoom: _rooms,
       dataType: _customerTypes || [],
+      dataCustomer: _customers,
       curUser: req.curUser
     })
   } catch (error) {
@@ -40,29 +42,36 @@ router.get("/views", async (req, res) => {
 
 router.post("/add", async (req, res) => {
   try {
-    const sur = Surcharge.findOne({isEnabled: true})
     const createdBy = "5f02c588e88cb9194897288d" // id employee or admin
-    const { customerId, roomId, dayCheckIn, dayCheckOut, numberOfDays, numberOfPeople, surchargeId, customerTypeId } = req.body;
+    const { customerId, roomId, dayCheckIn, dayCheckOut, numberOfPeople, customerTypeId } = req.body;
+    const sur = Surcharge.findOne({isEnabled: true}) // Surcharge chua lay dc data, xem lai!!!!!
     const room = await Room.findById(roomId);
-    let priceRoom = room.price;
-    if (!priceRoom) {
-      const cateData = await RoomCategory.findById(room.cateOfRoomId);
-      priceRoom = cateData.price;
-    }
+
     const _roomletter = new RoomLetter({})
     _roomletter.customerId = customerId
     _roomletter.roomId = roomId
-    // _roomletter.dayCheckIn = dayCheckIn
     _roomletter.dayCheckIn = Date.parse(dayCheckIn)
     _roomletter.dayCheckOut = Date.parse(dayCheckOut)
+    // NumberOfDays
     const diffTime = Math.abs(Date.parse(dayCheckOut) - Date.parse(dayCheckIn));
     _roomletter.numberOfDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
     _roomletter.numberOfPeople = numberOfPeople
     _roomletter.surchargeId = sur._id
     _roomletter.customerTypeId = customerTypeId
     _roomletter.createdBy = createdBy
-    if (numberOfPeople > sur.numberOfPeople) {
+    // Price
+    let priceRoom = room.price;
+    if (!priceRoom) {
+      const cateData = await RoomCategory.findById(room.cateOfRoomId);
+      priceRoom = cateData.price;
+      console.log("Vo if dau tien");
+
+    } else {
+      if (numberOfPeople >= sur.numberOfPeople) {
         _roomletter.price = priceRoom + sur.surchargePercent * priceRoom / 100;
+      } else {
+        _roomletter.price = priceRoom;
+      }
     }
 
     await _roomletter.save()
@@ -75,7 +84,9 @@ router.post("/add", async (req, res) => {
 
 router.post("/update", async (req, res) => {
   try {
-    const { id, customerId, roomId, dayCheckIn, dayCheckOut, numberOfDays, numberOfPeople, surchargeId, customerTypeId } = req.body
+    const { id, customerId, roomId, dayCheckIn, dayCheckOut, numberOfPeople, customerTypeId } = req.body
+    const sur = Surcharge.findOne({isEnabled: true}) // Surcharge chua lay dc data, xem lai!!!!!
+    const room = await Room.findById(roomId);
     if (!id) {
       return
     }
@@ -85,10 +96,27 @@ router.post("/update", async (req, res) => {
       if (roomId) _roomletter.roomId = roomId
       if (dayCheckIn) _roomletter.dayCheckIn = Date.parse(dayCheckIn)
       if (dayCheckOut) _roomletter.dayCheckOut = Date.parse(dayCheckOut)
-      if (numberOfDays) _roomletter.numberOfDays = numberOfDays
+      // NumberOfDays
+      const diffTime = Math.abs(Date.parse(dayCheckOut) - Date.parse(dayCheckIn));
+      _roomletter.numberOfDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
       if (numberOfPeople) _roomletter.numberOfPeople = numberOfPeople
-      if (surchargeId) _roomletter.surchargeId = surchargeId
       if (customerTypeId) _roomletter.customerTypeId = customerTypeId
+      _roomletter.surchargeId = sur._id
+      // Price
+      let priceRoom = room.price;
+      if (!priceRoom) {
+        const cateData = await RoomCategory.findById(room.cateOfRoomId);
+        priceRoom = cateData.price;
+        console.log("Vo if dau tien");
+
+      } else {
+        if (numberOfPeople >= sur.numberOfPeople) {
+          _roomletter.price = priceRoom + sur.surchargePercent * priceRoom / 100;
+        } else {
+          _roomletter.price = priceRoom;
+        }
+      }
+      
       await _roomletter.save()
     }
     return res.redirect("/room-letters/views")
